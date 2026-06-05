3c72f832ddbb544f8bdf618d7becc82dd5e41739
Luzzie-ux <estelarodrigoluz@gmail.com>
2026-06-05 22:15:46 +0100

    works now

---
+++ m1/ex0/ft_garden_intro.py
@@ -0,0 +1,17 @@
+#ft_garden_intro.py
+
+"""ft_garden_intro.py"""
+def	ft_garden_intro():
+    "prints garden info"
+    print("=== Welcome to My Garden ===")
+    print("Plant: Rose")
+    print("Height: 25cm")
+    print("Age: 30 days\n")
+    print("=== End of Program ===")
+
+def main():
+    """main"""
+    ft_garden_intro()
+
+if __name__ == "__main__":
+    main()
