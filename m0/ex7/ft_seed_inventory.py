a1df00c5d26ed678c26fbced34363ce60558c323
Luzzie <estelarodrigoluz@gmail.com>
2026-06-26 10:30:44 +0100

    reformted

--- m0/ex7/ft_seed_inventory.py
+++ m0/ex7/ft_seed_inventory.py
@@ -10,14 +10,20 @@ def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
     function that displays seed inventory with inputs from stdin
     """
     if unit.lower() == "packets":
-        print(f"{seed_type.capitalize()} seeds: "
-              f"{quantity} {unit.lower()} avaiable")
+        print(
+            f"{seed_type.capitalize()} seeds: "
+            f"{quantity} {unit.lower()} avaiable"
+        )
     elif unit.lower() == "grams":
-        print(f"{seed_type.capitalize()} seeds: "
-              f"{quantity} {unit.lower()} total")
+        print(
+            f"{seed_type.capitalize()} seeds: "
+            f"{quantity} {unit.lower()} total"
+        )
     elif unit.lower() == "area":
-        print(f"{seed_type.capitalize()} seeds: "
-              f"covers {quantity} square meters")
+        print(
+            f"{seed_type.capitalize()} seeds: "
+            f"covers {quantity} square meters"
+        )
     else:
         print("Unknown unit type")
     return
