7e865494aaa847e115e32d54a7576f44a3a726ee
Luzzie <estelarodrigoluz@gmail.com>
2026-06-26 09:54:44 +0100

    its works right now

--- m0/ex7/ft_seed_inventory.py
+++ m0/ex7/ft_seed_inventory.py
@@ -10,11 +10,25 @@ def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
     function that displays seed inventory with inputs from stdin
     """
     if unit.lower() == "packets":
-        print(f"{seed_type} seeds: {quantity} {unit.lower()} avaiable")
+        print(f"{seed_type.capitalize()} seeds: "
+              f"{quantity} {unit.lower()} avaiable")
     elif unit.lower() == "grams":
-        print(f"{seed_type} seeds: {quantity} {unit.lower()} total")
+        print(f"{seed_type.capitalize()} seeds: "
+              f"{quantity} {unit.lower()} total")
     elif unit.lower() == "area":
-        print(f"{seed_type} seeds: covers {quantity} square meters")
+        print(f"{seed_type.capitalize()} seeds: "
+              f"covers {quantity} square meters")
     else:
         print("Unknown unit type")
     return
+
+
+def main() -> None:
+    type: str = str(input("Choose a seed type: "))
+    quantity: int = int(input("Choose a qunatity: "))
+    unit: str = str(input("Choose a unit: "))
+    ft_seed_inventory(type, quantity, unit)
+
+
+if __name__ == "__main__":
+    main()
