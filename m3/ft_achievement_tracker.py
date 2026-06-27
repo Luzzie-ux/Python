934223bd0ca675802f1f24370bc6640eb5717540
Luzzie <estelarodrigoluz@gmail.com>
2026-06-27 20:27:35 +0100

    save

---
+++ m3/ft_achievement_tracker.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+Directory: ex3/
+Files to Submit: ft_achievement_tracker.py
+Authorized: len(), print(), import random, random.*, set(), set.union(),
+set.intersection(), set.difference()
+"""
+
+
+def ft_achievement_tracker() -> None:
+    return
+
+
+if __name__ == "__main__":
+    ft_achievement_tracker()
---
+++ searcher.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+
+"""
+Searching implementation in python
+"""
+
+from sys import argv
+from pathlib import Path
+
+
+def help() -> None:
+    print(
+        "usage: ./searcher [option] ... [file]\n"
+        "Version: 1.0.0 \n"
+        "Options:\n"
+        " --help:                       Displays this information.\n"
+        " --file [file]:                Displays the contents of [file(s)] in the terminal\n"
+        " --find [str]...:           Prints the number of occurrences of [str] in [file(s)]\n"
+        "\n"
+        "[nothing here yet]\n"
+    )
+    return
+
+
+def read_file(str_to_find: str, file: str) -> int:
+    count: int = 0
+    with open(file, "r") as f:
+        line: str = f.readline()
+        while line:
+            count += line.count(str_to_find)
+            line = f.readline()
+    return count
+
+def parser(args: list[str]) -> None:
+    if args[0] == "--help":
+        return help()
+    elif args[0] == "--file":
+        for file in args[1:]:
+            f = Path(file)
+            if f.exists():
+                with open(file, "r") as fd:
+                    print(f"{fd.read()}\n")
+            else:
+                return print(f"Could not find file {file}")
+        return
+    elif args[0] == "--find":
+        for file in args[2:]:
+            f = Path(file)
+            if f.exists():
+                occurences: int = read_file(args[1], file)
+                print(f"{occurences} occurrences of: '{args[1]}' in {file}")
+            else:
+                return print(f"Could not find file {file}")
+        return
+
+
+def main() -> None:
+    if len(argv) < 2:
+        return
+    try:
+        parser(argv[1:])
+    except (Exception, IndexError) as e:
+        print(f"{e.__class__.__name__}: {e}")
+    return
+
+
+if __name__ == "__main__":
+    main()
