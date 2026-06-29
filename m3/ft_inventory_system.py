72ffd53829c6af32dc4556fee2bc1f8452bd238b
Luzzie <estelarodrigoluz@gmail.com>
2026-06-29 17:31:58 +0100

    save

---
+++ m3/ft_inventory_system.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+
+"""
+Directory: ex4/
+Files to Submit: ft_inventory_system.py
+Authorized: import sys, sys.argv, len(), print(), sum(), list(), round(),
+dict.keys(), dict.values(), dict.update()
+"""
+
+def main() -> None:
+    return
+
+
+if __name__ == "__main__":
+    main()
