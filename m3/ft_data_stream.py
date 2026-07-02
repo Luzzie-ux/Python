6683ed54900e306004839854eb44c397e2fa9387
Luzzie <estelarodrigoluz@gmail.com>
2026-07-02 16:00:30 +0100

    save

--- m3/ft_data_stream.py
+++ m3/ft_data_stream.py
@@ -12,7 +12,41 @@ import typing
 import random
 
 
+PLAYERS: list = ["alice", "bob", "charlie", "dylan"]
+ACTIONS: list = [
+    "run",
+    "eat",
+    "sleep",
+    "grab",
+    "move",
+    "climb",
+    "swim",
+    "use",
+    "release",
+]
+
+
+def gen_event() -> typing.Generator[tuple[str, str], None, None]:
+    while True:
+        yield random.choice(PLAYERS), random.choice(ACTIONS)
+
+
+def ft_data_stream() -> None:
+    print("=== Game Data Stream Processor ===")
+    name: tuple[str]
+    move: tuple[str]
+    for i in range(6): #trocar pra 1000
+        name, move = next(gen_event())
+        print(f"Event {i}: Player {name} did action {move}")
+    stream: list[tuple[tuple[str], tuple[str]]] = []
+    for i in range(11):
+        stream.extend(next(gen_event()))
+    print(f"Built list of 10 events: {stream}")
+    return
+
+
 def main() -> None:
+    ft_data_stream()
     return
 
 
