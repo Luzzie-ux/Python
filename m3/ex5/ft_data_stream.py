2734e207857126533eaa4a24d629c32531d076f6
Luzzie <estelarodrigoluz@gmail.com>
2026-07-06 20:21:11 +0100

    save

--- m3/ex5/ft_data_stream.py
+++ m3/ex5/ft_data_stream.py
@@ -11,54 +11,43 @@ typing.Generator, import random, random.*
 import typing
 import random
 
-PLAYERS: list[str] = ["alice", "bob", "charlie", "dylan"]
-ACTIONS: list[str] = [
-    "run",
-    "eat",
-    "sleep",
-    "grab",
-    "move",
-    "climb",
-    "swim",
-    "use",
-    "release",
+ACTIONS: list[str] = ["eat", "sleep", "run", "move", "grab", "climb", "swim"]
+
+PLAYERS: list[str] = [
+    "Alice",
+    "Bob",
+    "Charlie",
+    "Dylan",
 ]
 
 
-def gen_event() -> typing.Generator[tuple[str, str], None, None]:
+def gen_events() -> typing.Generator[tuple[str, str], None, None]:
     while True:
         yield random.choice(PLAYERS), random.choice(ACTIONS)
 
 
-def con_event(
-    lista: list[tuple[str, str]],
+def consume_event(
+    events: list[tuple[str, str]],
 ) -> typing.Generator[tuple[str, str], None, None]:
-    while len(lista) > 0:
-        event: tuple[str, str] = random.choice(lista)
-        lista.remove(event)
+    while len(events) > 0:
+        event: tuple[str, str] = random.choice(events)
+        events.remove(event)
         yield event
 
 
-def ft_data_stream() -> None:
+def main() -> None:
     print("=== Game Data Stream Processor ===")
-    stream: typing.Generator[tuple[str, str], None, None] = gen_event()
-    name: str
-    move: str
-    for i in range(1000):
-        name, move = next(stream)
-        print(f"Event {i}: Player {name} did action {move}")
+    gen: typing.Generator[tuple[str, str], None, None] = gen_events()
+    for i in range(0, 1000):
+        event: tuple[str, str] = next(gen)
+        print(f"Event {i}: Player {event[0]} did action {event[1]}")
     events: list[tuple[str, str]] = []
     for i in range(0, 10):
-        events.append(next(stream))
+        events.append(next(gen))
     print(f"Built list of 10 events: {events}")
-    for event in con_event(events):
+    for event in consume_event(events):
         print(f"Got event from list: {event}")
-        print(f"Remains in list: {events}")
-    return
-
-
-def main() -> None:
-    ft_data_stream()
+        print(f"Remains in list {events}")
     return
 
 
