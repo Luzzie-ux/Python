ced3833ecd10f0c17de131191cc4bedc4a97e8fc
Luzzie <estelarodrigoluz@gmail.com>
2026-07-09 19:46:38 +0100

    module 5 initialized

---
+++ m5/data_pipeline.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+"""
+Directory: ex2/
+Files to Submit: data_pipeline.py
+Authorized: builtins, standard types, import typing, import abc
+"""
+
+import typing
+import abc
+
+
+def main() -> None:
+    return
+
+
+if __name__ == "__main__":
+    main()
---
+++ m5/data_stream.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+"""
+Directory: ex1/
+Files to Submit: data_stream.py
+Authorized: builtins, standard types, import typing, import abc
+"""
+
+import typing
+import abc
+
+
+def main() -> None:
+    return
+
+
+if __name__ == "__main__":
+    main()
