7fe4256365a9ca8fe415984674e97173df0dd9d7
Luzzie <estelarodrigoluz@gmail.com>
2026-07-31 10:46:57 +0100

    started ex0 of module 8

---
+++ m8/ex0/construct.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+"""
+Directory: ex0/
+Files to Submit: construct.py
+Authorized: sys, os, site modules, print()
+"""
+
+
+def main() -> None:
+    
+    print()
+    return
+
+
+if __name__ == "__main__":
+    main()
