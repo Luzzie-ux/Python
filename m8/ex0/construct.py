94f438a156af0a1ad06a020d9386268916ab6690
Luzzie <estelarodrigoluz@gmail.com>
2026-07-31 13:37:44 +0100

    save

--- m8/ex0/construct.py
+++ m8/ex0/construct.py
@@ -7,25 +7,32 @@ Files to Submit: construct.py
 Authorized: sys, os, site modules, print()
 """
 
-import sys, os
+import sys, os, site
+
+
+def is_venv() -> bool:
+    if sys.prefix == sys.base_exec_prefix:
+        return True
+    return sys.prefix != sys.base_prefix
 
 
 def main() -> int:
     os.write(1, b"=== Isolated Enviroment Checker ===\n")
-    if sys.prefix != sys.base_prefix:
-        return sys.stdout.write(
+    if is_venv():
+        sys.stdout.write(
             "Hello There! You are running this script in a venv!\n"
             f" we can know that because your sys.prefix: {sys.prefix}\n"
             f" is not the same as sys.base_prefix: {sys.base_prefix}\n"
         )
-    sys.stdout.write("Hello There! You are running this script without a venv!\n")
-    print(
-        "In case you want to use one, "
-        "type into your terminal the following:\n"
-        "\n   python3 -m venv <name_of_venv>\n"
-        "   source <name_of_venv>/bin/activate\n"
-        "\n(name_of_venv here is a user given name)"
-    )
+    else:
+        sys.stdout.write(
+            "Hello There! You are running this script without a venv!\n"
+            "In case you want to use one, "
+            "type into your terminal the following:\n"
+            "\n   python3 -m venv <name_of_venv>\n"
+            "   source <name_of_venv>/bin/activate\n"
+            "\n(name_of_venv here is a user given name)\n"
+        )
     print("Have a good day!")
 
 
