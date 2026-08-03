ec6a92c82ba15199f76087de96b1ab2a2de6deb0
Luzzie <estelarodrigoluz@gmail.com>
2026-08-03 13:18:36 +0100

    save changes

---
+++ m8/ex1/loading.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+
+"""
+Directory: ex1/
+Files to Submit: loading.py, requirements.txt, pyproject.toml
+Authorized: pandas, requests, matplotlib, numpy, sys, importlib
+"""
+
+import importlib
+import sys
+import pandas
+import requests
+import numpy
+import matplotlib
+
+def main() -> None:
+    return
+
+
+if __name__ == "__main__":
+    main()
