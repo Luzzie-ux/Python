bcb19a28d27f68ca6b7a21842717d57b33c2b016
Luzzie <estelarodrigoluz@gmail.com>
2026-08-03 19:44:16 +0100

    save

--- m8/ex1/loading.py
+++ m8/ex1/loading.py
@@ -42,6 +42,25 @@ def main() -> None:
     print(df2)
     print(df2.dtypes)
     print()
+    print(mpl.__name__)
+    print(plt.__name__)
+    print("<picture 1 goes here>")
+    fig, ax = plt.subplots()
+    ax.plot([1, 2, 3, 4], [1, 4, 2, 3])
+    plt.show()
+    np.random.seed(19680801)  # seed the random number generator.
+    data = {'a': np.arange(50),
+            'c': np.random.randint(0, 50, 50),
+            'd': np.random.randn(50)}
+    data['b'] = data['a'] + 10 * np.random.randn(50)
+    data['d'] = np.abs(data['d']) * 100
+
+    fig, ax = plt.subplots(figsize=(5, 2.7), layout='constrained')
+    ax.scatter('a', 'b', c='c', s='d', data=data)
+    ax.set_xlabel('entry a')
+    ax.set_ylabel('entry b')
+    plt.show()
+    print("<picture 2 goes here>")
     print(np.__name__)
     return
 
