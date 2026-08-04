932376ac765d6c92f4bc4f41d091add62c2fe6f3
Luzzie <estelarodrigoluz@gmail.com>
2026-08-04 18:34:27 +0100

    save

---
+++ m8/ex2/oracle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+"""
+Directory: ex2/
+Files to Submit: oracle.py, requirements.txt, .env.example, .gitignore
+Authorized: os, sys, python-dotenv modules, file operations
+"""
+
+import sys
+import os
+import dotenv
+
+
+def main() -> None:
+
+    return
+
+
+if __name__ == "__main__":
+    main()
