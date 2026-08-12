404402c6f70fc6d2e50ddae07fd2a28273c6bf69
Luzzie <estelarodrigoluz@gmail.com>
2026-08-12 19:43:45 +0100

    save

--- m10/ex0/lambda_spells.py
+++ m10/ex0/lambda_spells.py
@@ -2,43 +2,58 @@
 
 
 """
-lambda_spells.py
+Directory: ex0/
+Files to Submit: lambda_spells.py
+Authorized: map, filter, sorted, min, max, round, sum, len
 """
-
-import typing
-import collections.abc
-import itertools
-import functools
-import operator
 
 
 def artifact_sorter(artifacts: list[dict]) -> list[dict]:
-    return artifacts
+    return sorted(artifacts, key=lambda a: a["power"], reverse=True)
 
 
-def power_filter(mages: list[dict], min_power: int) -> list[dict]: ...
+def power_filter(mages: list[dict], min_power: int) -> list[dict]:
+    return list(filter(lambda p: p["power"] >= min_power, mages))
 
 
-def spell_transformer(spells: list[str]) -> list[str]: ...
+def spell_transformer(spells: list[str]) -> list[str]:
+    return list(map(lambda s: f"* {s} *", spells))
 
 
-def mage_stats(mages: list[dict]) -> dict: ...
+def mage_stats(mages: list[dict]) -> dict:
+    max(mages, key=lambda m: m["power"])["power"]
+    min(mages, key=lambda m: m["power"])["power"]
+    sum(mages, key=lambda m: m["power"]) / len(mages)
+    return {}
 
 
 def main() -> None:
     artifacts = [
-        {
-           "name" :"Fire Staff",
-           "power":92,
-           "type":"Fire",
-        },
-        {
-            "name":"Crystal Orb",
-            "power":85,
-            "type":"Crystal",
-        },
+        {"name": "Crystal Orb", "power": 114, "type": "weapon"},
+        {"name": "Lightning Rod", "power": 112, "type": "relic"},
+        {"name": "Ice Wand", "power": 62, "type": "armor"},
+        {"name": "Earth Shield", "power": 100, "type": "accessory"},
     ]
+    mages = [
+        {"name": "Alex", "power": 70, "element": "shadow"},
+        {"name": "Phoenix", "power": 77, "element": "lightning"},
+        {"name": "Nova", "power": 52, "element": "earth"},
+        {"name": "Jordan", "power": 76, "element": "light"},
+        {"name": "Rowan", "power": 54, "element": "wind"},
+    ]
+    spells = ["flash", "earthquake", "fireball", "tornado"]
+    print("\nTesting artifact sorter...")
     print(artifact_sorter(artifacts))
+
+    print("\nTesting Power Filter...")
+    print(power_filter(mages, 60))
+
+    print("\nTesting spell transformer...")
+    print(spell_transformer(spells))
+
+    print("\nTesting Mage Stats")
+    print(mage_stats(mages))
+
     return
 
 
